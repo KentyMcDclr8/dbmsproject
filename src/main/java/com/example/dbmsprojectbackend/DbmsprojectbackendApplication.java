6619d640bb710816409e9a1843cc85e3e01588ed
Amirreza Khoshbakht <amirrezaa.khoshbakht@gmail.com>
2022-12-24 18:33:11 +0300

    initial springboot application

---
+++ src/main/java/com/example/dbmsprojectbackend/DbmsprojectbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbmsprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmsprojectbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbmsprojectbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dbmsprojectbackend/DbmsprojectbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbmsprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbmsprojectbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
