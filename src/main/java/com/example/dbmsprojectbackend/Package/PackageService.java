c8f04b372a326ab602f3384755a6c7ad0d7f6b14
Faaiz Khan <faaizkhan4444@gmail.com>
2022-12-27 03:40:07 +0300

    adding default status to package

--- src/main/java/com/example/dbmsprojectbackend/Package/PackageService.java
+++ src/main/java/com/example/dbmsprojectbackend/Package/PackageService.java
@@ -60,12 +60,13 @@ public class PackageService {
             pack.packageId++;
             recipientOptionalId = packageRepository.findPackageById(pack.packageId);
         }
-        entityManager.createNativeQuery("INSERT INTO package (id, volume, weight, type, sent_by) VALUES (?, ?, ?, ?, ?)")
+        entityManager.createNativeQuery("INSERT INTO package (id, volume, weight, type, status, sent_by) VALUES (?, ?, ?, ?, ?, ?)")
                 .setParameter(1, pack.packageId)
                 .setParameter(2, pack.getVolume())
                 .setParameter(3, pack.getWeight())
                 .setParameter(4, pack.getType())
-                .setParameter(5, customer)
+                .setParameter(5, "to be assigned")
+                .setParameter(6, customer)
                 .executeUpdate();
         pack.packageId++;
     }
